a9f5c61077a32d0a0d6e3623a26063f108a19f10
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2020-05-08 01:31:28 +0630

    Add custom attribute to RoundedImageView

--- PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/RoundedCornerImageView.kt
+++ PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/RoundedCornerImageView.kt
@@ -13,15 +13,22 @@ class RoundedCornerImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    private var cornerRadius = 32f
+    private var cornerRadius = 0f
 
     private val path = Path()
 
+    init {
+
+        context.withStyledAttributes(attrs, R.styleable.RoundedCornerImageView) {
+            cornerRadius = getDimension(R.styleable.RoundedCornerImageView_cornerRadius, 0f)
+        }
+    }
+
     override fun onDraw(canvas: Canvas?) {
 
-        val rectangle = RectF(0f,0f,width.toFloat(),height.toFloat())
+        val rectangle = RectF(0f, 0f, width.toFloat(), height.toFloat())
 
-        path.addRoundRect(rectangle,cornerRadius,cornerRadius,Path.Direction.CCW)
+        path.addRoundRect(rectangle, cornerRadius, cornerRadius, Path.Direction.CCW)
 
         canvas?.clipPath(path)
 
