70c1a60c57666fd5285bbbf3419d3e6eda4249b8
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2020-05-09 01:31:31 +0630

    -Draw FanControlView ( Custom View)

--- PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/DialView.kt
+++ PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/DialView.kt
@@ -1,15 +1,15 @@
 package com.padcmyanmar.padcx.padc_x_recyclerview_ypst.veiws.components
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.PointF
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
 
-// 1 To represent the available fan speeds.
+// To represent the available fan speeds.
 private enum class FanSpeed(val label: Int) {
     OFF(R.string.fan_off),
     LOW(R.string.fan_low),
@@ -17,11 +17,9 @@ private enum class FanSpeed(val label: Int) {
     HIGH(R.string.fan_high);
 }
 
-// 2
 private const val RADIUS_OFFSET_LABEL = 30      // position for label
 private const val RADIUS_OFFSET_INDICATOR = -35 // position for circle indicator
 
-//3
 private var radius = 0.0f                   // Radius of the circle.
 private var fanSpeed = FanSpeed.OFF         // The active selection.
 
@@ -45,8 +43,76 @@ class DialView @JvmOverloads constructor(
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    // 1  to calculate the current radius of the dial's circle element
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        radius = (min(width, height) / 2.0 * 0.8).toFloat()
+
+    }
+
+    /**
+     * 2
+     * @param pos is the fan speed (OFF,LOW,MEDIUM,HIGH)
+     * @param radius is the whole view's radius
+     *
+     * calculates the X, Y coordinates on the screen for the text label and current indicator (0, 1, 2, or 3)
+     */
+    private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
+        // Angles are in radians.
+        pos.ordinal
+        val startAngle = Math.PI * (9 / 8.0)
+        val angle = startAngle + pos.ordinal * (Math.PI / 4)
+        x = (radius * cos(angle)).toFloat() + width / 2
+        y = (radius * sin(angle)).toFloat() + height / 2
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
+        //3
+        drawBackground(canvas)
+
+        //4
+        drawIndicatorCircle(canvas)
+
+        //5
+        drawTextLabels(canvas)
+    }
+
+    private fun drawBackground(canvas: Canvas) {
+
+        // Set dial background color to green if selection not off
+        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+
+        // Draw the circle
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
+
+    }
+
+    private fun drawIndicatorCircle(canvas: Canvas) {
+
+        // reduce radius -35 to make indicator inside of the circle
+        val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+
+        // get new position (x,y)
+        pointPosition.computeXYForSpeed(fanSpeed, markerRadius)
+
+        // set indicator background color
+        paint.color = Color.BLACK
+
+        canvas.drawCircle(pointPosition.x, pointPosition.y, radius / 12, paint)
+    }
+
+    private fun drawTextLabels(canvas: Canvas) {
+
+        // increase radius 30 to make labels outside of the circle
+        val labelRadius = radius + RADIUS_OFFSET_LABEL
+
+        for (i in FanSpeed.values()) {
+            pointPosition.computeXYForSpeed(i, labelRadius)
+            val label = resources.getString(i.label)
+            canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
+        }
     }
 
 }
